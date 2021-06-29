1c00194d2712cb4f78b7d17a808f11d3079d6531
Duc Ho <hotienduc05012002@gmail.com>
2021-06-29 14:13:10 +0700

    Added Footer
    Modified palette and create new Logo Component

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,33 @@
+import Link from "next/link";
+import Logo from "./Logo";
+
+import footerStyles from "../styles/Footer.module.scss";
+
 const Footer = ({}) => {
-  return <div>Footer</div>;
+  return (
+    <div className={footerStyles.footer}>
+      <div className={footerStyles.upper}>
+        <Logo />
+        <Link href="/">
+          <a className={footerStyles.mobile}>Mobile app</a>
+        </Link>
+      </div>
+      <div className={footerStyles.divisor}></div>
+      <div className={footerStyles.lower}>
+        <span></span>
+        <div className={footerStyles.tagline}>
+          © Club Day. 2021. Thương thương!
+        </div>
+        <div className={footerStyles.socialMediaContainer}>
+          <div>Theo dõi chúng tớ tại:</div>
+          <div className={footerStyles.socialMedias}>
+            <div className={footerStyles.socialMediaButtons}></div>
+            <div className={footerStyles.socialMediaButtons}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
