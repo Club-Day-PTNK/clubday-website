d30e44a5c9945f9d22cb9d18ab3a81d473752a1d
quốc huy <tahuy5952@gmail.com>
2021-07-10 14:42:59 +0700

    added login page and modify button and navigation

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,6 +1,8 @@
 import Input from "../../components/Input";
 import Link from "next/link";
 import Button from "../../components/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons"; 
 import loginStyles from "../../styles/Login.module.scss";
 const Login = () => {
   return (
@@ -8,7 +10,7 @@ const Login = () => {
       <div className={loginStyles.form}>
         <div className={loginStyles.title}>
           <span className={loginStyles.subtitle}>Chào mừng</span>
-          Đăng nhập
+          Đăng nhập ở đây
         </div>
         <div className={loginStyles.inputContainer}>
           <Input label="Email" name="email" />
@@ -24,7 +26,9 @@ const Login = () => {
         </div>
         <div className={loginStyles.buttonContainer}>
           <Button type="button" content="Đăng nhập" important={true}></Button>
-          <Button type="button" content="Sử dụng tài khoản Google" important={true}></Button>
+          <span className={loginStyles.buttonGoogle}>
+          <Button type="button" icon={<FontAwesomeIcon icon={faGoogle} size="2x" />} content="Sử dụng tài khoản Google" important={true}></Button>
+          </span>
         </div>
       </div>
     </div>
