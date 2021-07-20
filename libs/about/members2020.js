1b8bd713517b182f89c2ad5b3a6084d07bedade1
Quynh Nhu <quynhnhunguyenvo26082003@gmail.com>
2021-07-20 15:28:23 +0700

    components not yet finished

---
+++ libs/about/members2020.js
@@ -0,0 +1,90 @@
+export const memberContents = {
+    "Chương trình": {
+        "Quỳnh Như": {
+            classYear: "V1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Bảo Nghi": {
+            classYear: "KC1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Chánh Lộc": {
+            classYear: "H1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Minh Long": {
+            classYear: "H1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+    },
+    "Nội dung": {
+        "Phương Anh": {
+            classYear: "H1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Minh Thư": {
+            classYear: "KC1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Gia Tín": {
+            classYear: "KC1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+    },
+    "Hậu cần": {
+        "Tiến Đức": {
+            classYear: "I1720",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Ngọc Trâm": {
+            classYear: "H1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Viết Tín": {
+            classYear: "KC1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Trí Hân": {
+            classYear: "E1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+    },
+    "Đối ngoại": {
+        "Quỳnh Như": {
+            classYear: "V1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Quỳnh Lan": {
+            classYear: "S1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Thục Anh": {
+            classYear: "E1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Minh Thư": {
+            classYear: "KC1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+    },
+    "Thiết kế": {
+        "Văn Thanh": {
+            classYear: "V1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Mỹ Ngọc": {
+            classYear: "KC1922",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Thanh Bình": {
+            classYear: "KC1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+        "Phương Anh": {
+            classYear: "H1821",
+            icon: "/assets/images/Ellipse.svg",
+        },
+    },
+
+  };
+  
---
+++ pages/about/years-2020.js
@@ -0,0 +1,60 @@
+import years2020Styles from "../../styles/about/Years-2020.module.scss";
+import Hero from "../../components/about/Hero";
+import aboutStyles from "../../styles/About.module.scss";
+import Image from "next/image";
+import Member from "../../components/about/Member";
+import { memberContents } from "../../libs/about/members2020";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import Team from "../../components/about/Team";
+
+const year2020= () => {
+  return (
+    <div className={years2020Styles.container}>
+      <div className={aboutStyles.container}>
+        <Hero />
+      </div>
+      <div className={years2020Styles.body}>
+        <div className={years2020Styles.yearsContainer}>
+          <FontAwesomeIcon
+            icon={faTimes}
+            className={years2020Styles.icon}
+            onClick={() => {}}
+          />
+          <div className={years2020Styles.imageWrapper}>
+            <Image
+              src="/assets/images/Ellipse.svg"
+              alt="theme"
+              layout="fill"
+              objectPosition="center"
+            />
+          </div>
+          <h1 className={years2020Styles.title}>CLUB DAY 2020</h1>
+          <h3 className={years2020Styles.subtitle}>Blue</h3>
+          <p className={years2020Styles.para}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Hendrerit dignissim sapien urna consectetur amet, ipsum
+            porttitor porta nibh. Amet dui at etiam convallis. Nunc
+            congue sit egestas ac cursus nunc ac euismod enim. Ut
+            bibendum arcu, amet, donec malesuada odio faucibus. At
+            gravida diam vivamus amet. Sit aenean massa vel velit
+            velit adipiscing. Vitae, posuere cras nam duis nulla est.
+            Vel feugiat nisl amet et. Sed netus vel lorem cras nulla
+            vestibulum risus justo, vitae. Eget nibh dolor malesuada
+            risus scelerisque mauris purus pharetra.
+          </p>
+          <div className={years2020Styles.memberContainer}>
+           {Object.keys(memberContents).map((teamName) => (
+              <Team
+              teamName={teamName}
+              teamMembers={memberContents[teamName]}
+              />
+            ))}  
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default year2020;
